157fa9c81f278a72f4fe6a02b0e4e3446bd5b5bc
simone <simone.robutti@databiz.it>
2016-04-14 12:21:40 +0200

    Added custom :lines: functionality to remote-code-block

--- extensions/remote.py
+++ extensions/remote.py
@@ -1,20 +1,45 @@
 import requests
 import repos
 from sphinx.directives import code
-
-
+from sphinx.util import parselinenos
+from docutils.parsers.rst import Directive, directives
 class RemoteCodeBlock(code.CodeBlock):
     has_content = False
     required_arguments = 3
-    
+    optional_arguments = 0 
+    option_spec = {
+	'lines': directives.unchanged_required,
+        'caption':directives.unchanged_required
+	}
     def run(self):
+
+        document = self.state.document
+        env = document.settings.env
+        rel_filename, filename = env.relfn2path(self.arguments[0])
+
 	url = repos.repositories[self.arguments[1]]+"/"+self.arguments[2]
         r = requests.get(url)
-        if r.status_code != 200:
+        
+
+	
+	if r.status_code != 200:
             raise Exception("Failed to retrieve "+url)
 
-        self.content = [r.text]
+        lines = r.text.split("\n")
 
+	linespec = self.options.get('lines')
+  		
+        if linespec:
+            try:
+                linelist = parselinenos(linespec, len(lines))
+            except ValueError as err:
+                return [document.reporter.warning(str(err), line=self.lineno)]
+	    lines = [lines[i] for i in linelist if i < len(lines)]
+            if not lines:
+                return [document.reporter.warning(
+                    'Line spec %r: no lines pulled from include file %r' %
+                    (linespec, filename), line=self.lineno)]
+	self.content=lines
         return super(RemoteCodeBlock, self).run()
 
 

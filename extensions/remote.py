19da152087de767f6c25879debac738fcb14d1f0
simone <simone.robutti@databiz.it>
2016-04-14 17:07:58 +0200

    remote-code-block now trims whitespaces properly

--- extensions/remote.py
+++ extensions/remote.py
@@ -9,6 +9,7 @@ class RemoteCodeBlock(code.CodeBlock):
     optional_arguments = 0 
     option_spec = {
 	'lines': directives.unchanged_required,
+        'name':directives.unchanged_required,
         'caption':directives.unchanged_required
 	}
     def run(self):
@@ -18,7 +19,10 @@ class RemoteCodeBlock(code.CodeBlock):
         rel_filename, filename = env.relfn2path(self.arguments[0])
 
 	url = repos.repositories[self.arguments[1]]+"/"+self.arguments[2]
-        r = requests.get(url)
+
+	session=requests.Session()
+	session.headers.update({"Cache-Control": "no-cache, no-store, must-revalidate"})
+        r = session.get(url)
         
 
 	
@@ -35,12 +39,29 @@ class RemoteCodeBlock(code.CodeBlock):
             except ValueError as err:
                 return [document.reporter.warning(str(err), line=self.lineno)]
 	    lines = [lines[i] for i in linelist if i < len(lines)]
+	    
             if not lines:
                 return [document.reporter.warning(
                     'Line spec %r: no lines pulled from include file %r' %
                     (linespec, filename), line=self.lineno)]
+	
+	lines=self.custom_trim(lines)
 	self.content=lines
+        
+
         return super(RemoteCodeBlock, self).run()
+    
+    def custom_trim(self,lines):
+	
+	minwsnum=100
+	for line in lines:
+		wsnum=len(line) - len(line.lstrip()
+		)
+		if minwsnum>wsnum and len(line.lstrip())>0:
+	        	minwsnum=wsnum
+	lines=[line[minwsnum:] for line in lines]	
+	
+        return lines
 
 
 def setup(app):

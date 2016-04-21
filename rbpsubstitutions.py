4e2120ac99b325b946fd5c784d303a1bc1a6abac
Giampaolo Trapasso <giampaolo.trapasso@radicalbit.io>
2016-04-21 11:01:18 +0200

    Added initial Cassandra documentation

--- rbpsubstitutions.py
+++ rbpsubstitutions.py
@@ -1,8 +1,10 @@
 versions_subs="""
 .. |kafka-version| replace:: 0.9.0.1
 .. |kafka-connector-version| replace:: 0.9
+.. |cassandra-version| replace:: 2.2.5
 """
 
 other_substitutions="""
+.. |fdd| replace:: Radicalbit Fast Data Distribution
 """
 all_substitutions=versions_subs+other_substitutions

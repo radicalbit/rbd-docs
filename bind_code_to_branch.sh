40dd7f85c449af7b33b5c0d2e2907349b399b3ef
chobeat <simone.robutti@gmail.com>
2016-04-26 15:27:49 +0200

    Added support script for submodule management

---
+++ bind_code_to_branch.sh
@@ -0,0 +1,1 @@
+git config -f .gitmodules submodule.rbp-docs-code.branch $1
---
+++ checkout_code_branch.sh
@@ -0,0 +1,1 @@
+git submodule foreach -q --recursive 'git checkout $(git config -f $toplevel/.gitmodules submodule.$name.branch || echo master)'
